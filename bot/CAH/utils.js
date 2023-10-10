7173ce72d302cb7e2cd11464fb032d1815e14264
plizeee <plizman6@gmail.com>
2023-10-10 11:46:19 -0300

    added utils.js utility functions

---
+++ bot/CAH/utils.js
@@ -0,0 +1,25 @@
+const readlineSync = require('readline-sync'); // Using 'readline-sync' for synchronous user input
+
+function displayCards(cards) {
+    cards.forEach((card, index) => {
+        console.log(`[${index + 1}] ${card.text}`);
+    });
+}
+
+function getUserInput(prompt) {
+    return readlineSync.question(prompt);
+}
+
+function randomizeArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // ES6 array element swap
+    }
+    return array;
+}
+
+module.exports = {
+    displayCards,
+    getUserInput,
+    randomizeArray
+};
