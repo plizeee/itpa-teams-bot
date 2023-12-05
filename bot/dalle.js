87096ba52392450ddcbdba88172ead3bbfe11568
Matthieu Savoie <plizman6@gmail.com>
2023-12-05 15:16:00 -0400

    added error message and revised prompt in output

--- bot/dalle.js
+++ bot/dalle.js
@@ -23,7 +23,7 @@ module.exports = {
 };
 
 async function dalleCommand(msg){
-    let message = msg.content.toUpperCase();
+    let message = msg.content;
     message = stripCommand(message);
 
     msg.channel.sendTyping();
@@ -43,20 +43,74 @@ async function dalleCommand(msg){
         console.log(err);
     });
 
+    // let response = null;
+
     if(!response){
         console.log("no response");
+        
+        //the user said something that triggered the moderation policy, so tell the ai to shame the user based on what they said
+        let fullPrompt = [];
+
+        fullPrompt.push({
+            role: "system",
+            content: "The following DALL-E prompt has been blocked because it violates the moderation policy. Let the user know this by shaming, teasing and making fun of them over their request."
+        });
+        fullPrompt.push({
+            role: "user",
+            content: message
+        });
+
+
+        request = {
+            model: "gpt-3.5-turbo-16k-0613",
+            messages: fullPrompt,
+            max_tokens: 500
+        }
+
+        let completion = await openai.chat.completions.create(request)
+        .catch(error => { //catch error 400 for bad request
+            console.log(error);
+            if(error.code == 'context_length_exceeded'){
+                msg.reply("Your message is too long. Please try again.");
+                
+            }
+        })
+        .catch(error => { //catching errors, such as sending too many requests, or servers are overloaded
+            console.log(error);
+            msg.reply("Something went wrong. Please try again later.");
+        });
+
+        if(!completion) {
+            console.log("completion is null");
+            return;
+        }
+
+        console.log(JSON.stringify(request, null, 2));
+        console.log(JSON.stringify(completion, null, 2));
+
+        let completionMessage = completion.choices[0].message
+
+        //send the message as a reply
+        await msg.reply(completionMessage.content);
+
         return;
     }
 
     let image_url = response.data[0].url;
     let filename = "reply.png";
 
+    let revised_prompt = response.data[0].revised_prompt;
+
     //send the image as a reply
     const attachment = new Discord.AttachmentBuilder(image_url);
 
     attachment.name = filename;
 
-    await msg.reply({ files: [attachment] });
+    //send the image as a reply with the message
+    await msg.reply({ 
+        content: revised_prompt,
+        files: [attachment] 
+    });
 }
 
 function stripCommand(message){
